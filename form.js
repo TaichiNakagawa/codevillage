f7fbca1fe4f8ed85b0805045b14fd35fd7f8f2aa
TaichiNakagawa <69260565+TaichiNakagawa@users.noreply.github.com>
2020-09-13 04:44:40 +0900

    Add files via upload

---
+++ form.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+// 関数型コンポーネント
+export  const Form = () => {
+    return (
+        <form>
+            <input type="text"/>
+        </form>
+    );
+}
+
+//export default Form;
+
+// exportの注意 : default の場合は、あとで描く
+// exportの注意 : defaultなし の場合は、最初に書く
---
+++ item.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Item = () => {
+  return(
+      <li>
+          <input type="checkbox" />
+          <span>Todoリストのタイトル</span>
+      </li>
+  )
+}
